84e95c0e629ed2992a95aa9b768cfef77d49c8d2
Sergey Stryuchkov <serega.stryuchkov@gmail.com>
2024-03-21 23:49:36 +0300

    update v1.0.1

--- config/dev.js
+++ config/dev.js
@@ -1,13 +1,14 @@
 const gulp = require('gulp');
 
 const fileInclude = require('gulp-file-include');
+const prettier = require('@bdchauvette/gulp-prettier');
 
 const sass = require('gulp-sass')(require('sass'));
 const sassGlob = require('gulp-sass-glob');
 const sourceMaps = require('gulp-sourcemaps');
 
 const changed = require('gulp-changed');
-const notify = require('gulp-notify');
+const changedInPlace = require('gulp-changed-in-place');
 const plumber = require('gulp-plumber');
 
 
@@ -29,34 +30,42 @@ const mode = 'development';
 
 
 gulp.task('html:dev', function () {
-    return gulp.src(['./src/html/pages/*.html', './src/html/*.html'])
-        .pipe(changed('./app/'), {
-            hasChanged: changed.compareContents
-        })
+    return gulp.src('./src/html/*.html')
         .pipe(plumber(settings.plumberNotify('HTML')))
         .pipe(fileInclude(settings.fileInclude))
+        .pipe(
+            prettier({
+                tabWidth: 4,
+                useTabs: true,
+                printWidth: 182,
+                trailingComma: 'es5',
+                bracketSpacing: false,
+            })
+        )
+
         .pipe(gulp.dest('./app/'))
+        .pipe(changedInPlace())
         .pipe(browserSync.stream());
 });
 
 gulp.task('sass:dev', function () {
     return gulp.src(['./src/scss/pages/*.scss', './src/scss/*.scss'])
-        .pipe(changed('./app/css/'))
         .pipe(plumber(settings.plumberNotify('SCSS')))
         .pipe(sourceMaps.init())
         .pipe(sassGlob())
         .pipe(sass())
         .pipe(sourceMaps.write())
         .pipe(gulp.dest('./app/css/'))
+        .pipe(changedInPlace())
         .pipe(browserSync.stream());
 })
 
 gulp.task('js:dev', function () {
     return gulp.src('./src/js/*.js')
-        .pipe(changed('./app/js/'))
         .pipe(plumber(settings.plumberNotify('JS')))
         .pipe(webpack(webpackConfig(mode)))
         .pipe(gulp.dest('./app/js'))
+        .pipe(changedInPlace())
         .pipe(browserSync.stream());
 })
 
@@ -81,10 +90,11 @@ gulp.task('image:dev', function () {
 gulp.task('watch', function () {
     browserSync.init({
         server: {
-            baseDir: './app'
-        },
+            baseDir: './app',
+        }
     });
-    gulp.watch(`./src/**/*.html`, gulp.parallel('html:dev'));
+
+    gulp.watch(`./src/html/**/*.html`, gulp.parallel('html:dev'));
     gulp.watch('./src/scss/**/*.scss', gulp.parallel('sass:dev'));
     gulp.watch('./src/js/**/*.js', gulp.parallel('js:dev'));
     gulp.watch('./src/resources/**', gulp.parallel('resources'));

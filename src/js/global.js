8e0a6482b7017cca250c58dcd79757d9652614a9
Sergey Stryuchkov <serega.stryuchkov@gmail.com>
2024-04-05 17:36:46 +0300

    Модуль тултип

--- src/js/global.js
+++ src/js/global.js
@@ -1,5 +1,6 @@
 import 'focus-visible';
 import Modal from './functions/modal';
+import Tooltip from './functions/tooltip';
 import './functions/fix-fullheight';
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -9,4 +10,24 @@ document.addEventListener('DOMContentLoaded', () => {
     const modalSettings = {};
 
     const modal = new Modal(modalSettings);
+
+    const tooltipHtml = new Tooltip({
+        mode: 'html',
+        gap: 10,
+        targetSelector: '[data-tooltip-html]',
+        elementSelector: 'tooltip-html',
+        animation: {
+            type: 'fade-up',
+            speed: 300,
+            transformGap: '10px'
+        },
+    });
+
+    const tooltip = new Tooltip({
+        mode: 'default',
+        gap: 10,
+        position: 'bottom',
+    });
+
+
 });
